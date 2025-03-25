16c4fec69346293546f66be9b069238f251e0dc3
Tatiana <tatianarieram@gmail.com>
2025-03-25 19:54:40 +0100

    Creación del Proyecto con Spring Boot

---
+++ BackEnd-SpringBoot/src/main/java/com/tfg/TatianaApplication.java
@@ -0,0 +1,13 @@
+package com.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatianaApplication.class, args);
+	}
+
+}
